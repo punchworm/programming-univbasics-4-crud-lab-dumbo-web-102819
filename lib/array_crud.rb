5bfa4438e3c2909c13893585ee9543401d7dadbe
Nia Laureano <nialaureano@icloud.com>
2019-10-24 17:39:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,34 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  numbs = ["one", "two", "three", "four"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  numbs = ["one", "two", "three", "four"]
+  numbs << "five"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  numbs = ["one", "two", "three", "four"]
+  numbs.unshift("zero")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    numbs = ["one", "two", "three", "four"]
+    numbs.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+      numbs = ["one", "two", "three", "four"]
+numbs.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+        numbs = ["one", "two", "three", "four"]
+numbs[0]
 end
 
 def retrieve_first_element_from_array(array)
